6a2bcf9f0581114e321ac785d06c58d21c9f9e37
timothek2112 <nikzav6080@gmail.com>
2023-06-09 03:43:08 +0700

    feature(building): building working, but need to fix z-order and angle

--- UI/BuildingListItem.cs
+++ UI/BuildingListItem.cs
@@ -8,8 +8,7 @@ using Newtonsoft.Json;
 public partial class BuildingListItem : Button
 {
 	[Export] public int id;
-	[Export] public int building_id;
-	public IBuildable building;
+	[Export] public PackedScene building;
 	[Export] BuildingTypesEnum buildingType;
 	public List<ResearchesEnum> researches = new List<ResearchesEnum>();
 
@@ -20,33 +19,12 @@ public partial class BuildingListItem : Button
 		if(self == null)
 			return;
 		researches = self.researches;
-		building_id = self.building_id;
 		buildingType = self.buildingType;
-		if(buildingType == BuildingTypesEnum.mine)
-		{
-			json = (Json)GD.Load("res://Tech/Config/Mines.tres");
-			var concreteBuildings = JsonConvert.DeserializeObject<List<Mine>>(json.Data.ToString());
-			var thisBuilding = concreteBuildings.FirstOrDefault(p => p.id == building_id);
-			if(thisBuilding == null)
-				return;
-			building = thisBuilding;
-		}
-		else{
-			json = (Json)GD.Load("res://Tech/Config/Storages.tres");
-			var concreteBuildings = JsonConvert.DeserializeObject<List<StorageBuilding>>(json.Data.ToString());
-			var thisBuilding = concreteBuildings.FirstOrDefault(p => p.id == building_id);
-			if(thisBuilding == null)
-				return;
-			building = thisBuilding;
-		}
-		
 	}
 
 	private void _on_button_down(){
-		var newBuilding = new PackedScene();
-		newBuilding.Pack((Node)building);
-		var instance = newBuilding.Instantiate();
-		AddChild(instance);
+		var instance = building.Instantiate();
+		GetNode("/root").AddChild(instance);
 		((BaseBuilding)instance).Builded = instance.GetNode<Node2D>("Builded");
 		((BaseBuilding)instance).Blueprint = instance.GetNode<Node2D>("Blueprint");
 		GetNode<PlayerController>("/root/PlayerController").EnableBuildingMode((IBuildable)instance);
